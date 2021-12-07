edb0fc4cf39145809d39dfa343c2ebd189aea40f
Brenon <brenonparry@gmail.com>
2021-12-07 04:40:49 +0000

    added body-parser to handle POST requests. Created a function for generating a random tiny url. Created a URL submission form.

--- express_server.js
+++ express_server.js
@@ -1,15 +1,33 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xvn2": "http://www.lighthouselabs.ca",
   "9sm5xk": "http://www.google.com"
 };
-// const photo = '/Users/brenonparry/lighthouse/w3/tinyapp/lloyd.jpg'
+
+const generateRandomString = function() {
+  return Math.random().toString(20).substring(2, 8)
+}
+console.log(generateRandomString());
+// ROUTES =>
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase }; 
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  
+  res.send("Ok");        
 });
 
 app.get("/urls/:shortURL", (req, res) => {
