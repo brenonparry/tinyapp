b56b47a0d55b5a0238cce816ccf77f74905380e9
Brenon <brenonparry@gmail.com>
2021-12-10 04:40:05 +0000

    Refactoring where needed and organizing to make more coherant

--- express_server.js
+++ express_server.js
@@ -13,10 +13,11 @@ app.set("view engine", "ejs");
 app.use(cookieParser());
 app.use(cookieSession({
     name: "session",
-    keys: ["I was robbed by a sweet old lady", "I didn't eve see it coming"],
+    keys: ["I was robbed by a sweet old lady", "I didn't even see it coming"],
   })
 );
 
+// URL DATABASE
 const urlDatabase = {
   "b2xvn2": {
     longURL: "http://www.lighthouselabs.ca",
@@ -27,6 +28,7 @@ const urlDatabase = {
     userID: "dd5ts4"
   }
 }
+// USER DATABASE
 const users = {
   "6h8dv3": {
     id: "6h8dv3", 
@@ -60,7 +62,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls/register", (req, res) => {
-  const id = req.cookies.user_id
+  const id = req.session.id
   const templateVars = { 
     user: users[id]
   };
@@ -68,27 +70,26 @@ app.get("/urls/register", (req, res) => {
 });
 
 app.post("/urls/register", (req, res) => {
-  const id = generateRandomString()
+  const id = generateRandomString() // calling helper function
   const email = req.body.email
   const password = req.body.password
-  const user = checkUserEmail(email, users);
+  const user = checkUserEmail(email, users); // calling helper function
   
-  if (!email || !password) {
+  if (!email || !password) { // Checking for blank fields 
     return res.status(400).send("email and/or password cannot be blank!");
   }
-  
-  if(user){
+  if(user){ // Checking for existing user
     return res.status(400).send("user already exists")
   }
+  // create hashed password
   const hashedPassword = bcrypt.hashSync(password, 10);
-
+  // create new user for
   users[id] = {
     id: id,
     email: email,
     password: hashedPassword
   }
-
-  // res.cookie("user_id", id)
+  // cookie time!
   req.session.id = id
   res.redirect("/urls")
 })
@@ -99,7 +100,7 @@ app.post("/urls/register", (req, res) => {
 app.get("/urls", (req, res) => {
   const id = req.session.id;
   const user = users[id]
-  const urls = urlsForUser(id)
+  const urls = urlsForUser(id) // calling helper function 
 
   const templateVars = {
     urls,
@@ -110,7 +111,7 @@ app.get("/urls", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  const randomURL = generateRandomString();
+  const randomURL = generateRandomString(); // calling helper function
   const id = req.session.id
 
   urlDatabase[randomURL] = {}
@@ -124,7 +125,7 @@ app.post("/urls", (req, res) => {
 app.get("/urls/new", (req, res) => {
   const id = req.session.id
 
-  if(!id) {
+  if(!id) { // checking for valid user id
    return res.redirect("/urls/login");
   }
 
@@ -152,34 +153,39 @@ app.post('/urls/login', (req, res) => {
   const password = req.body.password;
   const user = checkUserEmail(email, users)
   
-  if(!user){
+  if(!user){ // Checking for existing user email
     return res.status(400).send("incorect email")
   }
 
   const userPassword = user.password
   const passwordMatching = bcrypt.compareSync(password, userPassword);
 
-  if (!passwordMatching) {
+  if (!passwordMatching) { // checking for existing user password
     return res.status(400).send("password does not match")
   }
   
   const id = user.id
+  // cookie time!
   req.session.id = id
   res.redirect("/urls");
 });
 
+// LOGOUT & DELETE COOKIE
 app.post('/urls/logout', (req,res) => {
-  // res.clearCookie('user_id')
-  delete req.session.id
+  delete req.session.id // delete the cookie
   res.redirect("/urls/login");
 });
 
 //---------------------------------------------//
 
-// URLS SHOW
+// URLs SHOW
 app.get("/urls/:shortURL", (req, res) => {
-  const id = req.cookies.user_id
+  const id = req.session.id
   const shortURL = req.params.shortURL
+  
+  if(id !== urlDatabase[shortURL].userID) { // Checking if requesting user matches url owner
+    return res.status(403).send("ah ah ah, that doesnt belong to you!")
+  }
   
   const templateVars = { 
     shortURL: shortURL, 
@@ -203,19 +209,15 @@ app.get("/u/:shortURL", (req, res) => {
 // "SUBMIT NEW LONG URL" POST
 app.post('/urls/:shortURL/submit', (req,res) => {
   const shortURL = req.params.shortURL
-  // const id = req.cookies.user_id
-  console.log("I entered submit")
-  // urlDatabase[shortURL] = {}
   urlDatabase[shortURL].longURL = req.body.newURL
-  // urlDatabase[shortURL].userID = id
- 
+
   res.redirect("/urls");
 });
 
 // "EDIT URL" POST
 app.post('/urls/:shortURL', (req,res) => {
   const shortURL = req.params.shortURL
-  console.log("shortURL: ", shortURL)
+  
   res.redirect(shortURL);
 });
 
